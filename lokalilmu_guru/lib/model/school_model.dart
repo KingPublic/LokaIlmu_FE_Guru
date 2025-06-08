1fb62f8b5e99496f2e50d29babcae5bf223a47cc
Andrey Hartawan Suwardi <144876604+KingPublic@users.noreply.github.com>
2025-06-08 20:11:08 +0800

    yaudah_idkwhy_registersnya

---
+++ lokalilmu_guru/lib/model/school_model.dart
@@ -0,0 +1,11 @@
+class SchoolModel {
+  final int idSekolah;
+  final String npsn;
+  final String namaSekolah;
+
+  SchoolModel({
+    required this.idSekolah,
+    required this.npsn,
+    required this.namaSekolah,
+  });
+}
